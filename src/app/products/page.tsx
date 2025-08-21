afcff84d05efb5ca6bdfe76836384c546aabb5e5
athuljain <athuljainkj2@gmail.com>
2025-08-21 09:59:51 +0530

    first commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function Products(){
+    return(
+        <div>
+            <h1>Products</h1>
+
+            <h4>product 1</h4>
+            <h4>product 2</h4>
+            <h4>product 3</h4>
+        </div>
+    )
+}
